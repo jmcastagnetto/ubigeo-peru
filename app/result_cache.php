5b6e33f41b594e6070e33ec6978b96949247e084
Jesus M. Castagnetto <jesus@castagnetto.com>
2013-02-14 22:16:15 -0500

    compress cache only if server env var is set

--- app/result_cache.php
+++ app/result_cache.php
@@ -1,8 +1,14 @@
 <?php
+
+
 function get_from_cache( $key ) {
     $m = new Memcache();
     $m->connect('tunnel.pagodabox.com',11211);
-    $res = $m->get($key, MEMCACHE_COMPRESSED);
+    if ( $_SERVER['COMPRESS_CACHE'] === true ) {
+        $res = $m->get($key, MEMCACHE_COMPRESSED);
+    } else {
+        $res = $m->get($key);
+    }
     $m->close();
     return $res;
 }
@@ -10,7 +16,11 @@ function get_from_cache( $key ) {
 function save_to_cache( $key, $res ) {
     $m = new Memcache();
     $m->connect('tunnel.pagodabox.com',11211);
-    $op = $m->set($key, $res, MEMCACHE_COMPRESSED);
+    if ( $_SERVER['COMPRESS_CACHE'] === true ) {
+        $op = $m->set($key, $res, MEMCACHE_COMPRESSED);
+    } else {
+        $op = $m->set($key, $res);
+    }
     $m->close();
     return $op;
 }
