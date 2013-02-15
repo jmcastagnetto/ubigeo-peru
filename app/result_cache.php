47d74af2188e493b78eb9bbb61105c078c068369
Jesus M. Castagnetto <jesus@castagnetto.com>
2013-02-14 20:45:52 -0500

    switch to memcache

--- app/result_cache.php
+++ app/result_cache.php
@@ -1,14 +1,16 @@
 <?php
 function get_from_cache( $key ) {
-    $m = new Memcached('result_cache');
-    $m->addServer('tunnel.pagodabox.com',11211);
-    $res = $m->get($key);
+    $m = new Memcache());
+    $m->connect('tunnel.pagodabox.com',11211);
+    $res = $m->get($key, MEMCACHE_COMPRESSED);
+    $m->close();
     return $res;
 }
 
 function save_to_cache( $key, $res ) {
-    $m = new Memcached('result_cache');
-    $m->addServer('tunnel.pagodabox.com',11211);
-    $op = $m->set($key, $res);
+    $m = new Memcache();
+    $m->connect('tunnel.pagodabox.com',11211);
+    $op = $m->set($key, $res, MEMCACHE_COMPRESSED);
+    $m->close();
     return $op;
 }
