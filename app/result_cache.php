8856a3787ab0c470d3e6a4812ff15e867bc59f07
Jesus Castagnetto <jesus@castagnetto.com>
2015-05-29 17:36:38 +0000

    not using compression with memcached

--- app/result_cache.php
+++ app/result_cache.php
@@ -4,11 +4,7 @@
 function get_from_cache( $key ) {
     $m = new Memcache();
     $m->connect(getenv("CACHE2_HOST"),getenv("CACHE2_PORT"));
-   // if ( $_SERVER['COMPRESS_CACHE'] === true ) {
-//        $res = $m->get($key, MEMCACHE_COMPRESSED);
-  //  } else {
-        $res = $m->get($key);
-//    }
+    $res = $m->get($key);
     $m->close();
     return $res;
 }
@@ -16,11 +12,7 @@ function get_from_cache( $key ) {
 function save_to_cache( $key, $res ) {
     $m = new Memcache();
     $m->connect(getenv("CACHE2_HOST"),getenv("CACHE2_PORT"));
-  //  if ( $_SERVER['COMPRESS_CACHE'] === true ) {
-        //$op = $m->set($key, $res, MEMCACHE_COMPRESSED);
-    //} else {
-        $op = $m->set($key, $res);
-    //}
+    $op = $m->set($key, $res);
     $m->close();
     return $op;
 }
